ea64cdf76c21231883238215d816d4f06f86caaa
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-10-22 15:01:04 +0100

    Revert "creates function for obtaining student list"
    This reverts commit 1d98d6f2332d90bc9f326f7211f5cd1ff38870cf.

--- directory.rb
+++ directory.rb
@@ -1,22 +1,22 @@
 # let's put all students into an array
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
-end
-
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+# and then print them
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-
 def print(names)
   names.each_with_index do |name, index|
     if name[:name].length < 12
@@ -24,12 +24,10 @@ def print(names)
     end
   end
 end
-
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
 print_header
 print(students)
 print_footer(students)
