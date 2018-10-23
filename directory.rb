64e7ef11519c8675275f361ce78ee22938bc248b
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-10-23 12:16:12 +0100

    finish splitting into methods

--- directory.rb
+++ directory.rb
@@ -47,17 +47,17 @@ def print_footer
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
 def interactive_menu
   @students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    process(selection)
-    # 3. do what the user has asked
+    print_menu
+    process(gets.chomp)
   end
 end
 
